c8b6902aa83f65acceea6a4ad173dd69ddbc93d5
zeuss254 <ckarimi676@gmail.com>
2023-09-25 06:55:28 +0000

    update

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,93 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
+
+/**
+ * swap_ints - Swaps two integers in an array.
+ * @a: First integer to swap.
+ * @b: Second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * hoare_partition - Order a subset of an array of integers.
+ * @array: Array of integers.
+ * @size: Size of the array.
+ * @left: Starting index of the subset to order.
+ * @right: Ending index of the subset to order.
+ *
+ * Return: Final partition index.
+ *
+ * Description: Uses the last element of the partition as the pivot.
+ */
+int hoare_partition(int *array, size_t size, int left, int right)
+{
+	int pivot, up, down;
+
+	pivot = array[right];
+	for (up = left - 1, down = right + 1; up < down;)
+	{
+		do {
+			up++;
+		} while (array[up] < pivot);
+		do {
+			down--;
+		} while (array[down] > pivot);
+
+		if (up < down)
+		{
+			swap_ints(array + up, array + down);
+			print_array(array, size);
+		}
+	}
+
+	return (up);
+}
+
+/**
+ * hoare_sort - Implement the quicksort algorithm through recursion.
+ * @array: Array of integers to sort.
+ * @size: Size of the array.
+ * @left: Starting index of the array partition to order.
+ * @right: Ending index of the array partition to order.
+ *
+ * Description: Uses the Hoare partition scheme.
+ */
+
+void hoare_sort(int *array, size_t size, int left, int right)
+{
+	int partn;
+
+	if (right - left > 0)
+	{
+		partn = hoare_partition(array, size, left, right);
+		hoare_sort(array, size, left, partn - 1);
+		hoare_sort(array, size, partn, right);
+	}
+}
+
+/**
+ * quick_sort_hoare - Sort an array of integers in ascending.
+ * @array: Array of integers.
+ * @size: Size of the array.
+ *
+ * Description: Uses the Hoare partition scheme.
+ */
+
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	hoare_sort(array, size, 0, size - 1);
+}
